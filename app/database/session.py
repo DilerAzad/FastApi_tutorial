0bf3c19f27a3d2b71573247c0afd9624ce8778dd
DilerAzad <dilersinghazad@gmail.com>
2026-03-09 15:48:53 +0530

    complete 10

--- app/database/session.py
+++ app/database/session.py
@@ -1,23 +1,29 @@
-from typing import Annotated
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import SQLModel
 
-from fastapi import Depends
-from sqlalchemy import create_engine
-from sqlmodel import Session, SQLModel
+from app.config import settings
 
-engine = create_engine(
-    url="sqlite:///shipment.db", echo=True, connect_args={"check_same_thread": False}
+engine = create_async_engine(
+    url=settings.POSTGRES_URL(),
+    echo=True
 )
 
 
-def create_db_tables():
-    from .models import Shipment
+async def create_db_tables():
+    async with engine.begin() as conn:
+        from .models import Shipment
+        await conn.run_sync(SQLModel.metadata.create_all)
 
-    SQLModel.metadata.create_all(bind=engine)
 
+async_session =sessionmaker(
+        bind = engine,
+        class_ = AsyncSession,
+        expire_on_commit = False,
+    )
 
-def get_session():
-    with Session(bind=engine) as session:
+async def get_session():
+    
+    async with async_session() as session:
         yield session
 
-
-SessionDep = Annotated[Session, Depends(get_session)]
--- app/main.py
+++ app/main.py
@@ -1,71 +1,22 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, HTTPException, status
 from scalar_fastapi import get_scalar_api_reference
 
-from app.database.models import Shipment
-from app.database.session import SessionDep, create_db_tables
+from app.database.session import create_db_tables
 
-from .schemas import ShipmentCreate, ShipmentRead, ShipmentUpdate
+from app.api.router import router
 
+from fastapi import FastAPI
 
 @asynccontextmanager
 async def lifespan_handler(app: FastAPI):
-    create_db_tables()
+    await create_db_tables()
     yield
 
 
 app = FastAPI(lifespan=lifespan_handler)
 
-
-### Read a shipment by id
-@app.get("/shipment", response_model=ShipmentRead)
-def get_shipment(id: int, session: SessionDep):
-    shipment = session.get(Shipment, id)
-    if not shipment:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Shipment not found"
-        )
-    return shipment
-
-
-### Create a new shipment with content and weight
-@app.post("/shipment", response_model=dict[str, int])
-def submit_shipment(shipment: ShipmentCreate, session: SessionDep):
-    new_shipment = Shipment(**shipment.model_dump())
-    session.add(new_shipment)
-    session.commit()
-    session.refresh(new_shipment)
-
-    return {"id": new_shipment.id}
-
-
-### Update an existing shipment
-@app.patch("/shipment", response_model=ShipmentUpdate)
-def update_shipment(id: int, shipment_update: ShipmentUpdate, session: SessionDep):
-    update = shipment_update.model_dump(exclude_none=True)
-
-    if update is None:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="No data provided to update"
-        )
-    shipment = session.get(Shipment, id)
-    shipment.sqlmodel_update(update)  # ty:ignore[possibly-missing-attribute]
-
-    session.add(shipment)
-    session.commit()
-    session.refresh(shipment)
-    return shipment
-
-
-### Delete a Shipment
-@app.delete("/shipment", response_model=dict[str, str])
-def delete_shipment(id: int, session: SessionDep):
-    session.delete(session.get(Shipment, id))
-    session.commit()
-
-    return {"detail": f"Shipment with id #{id} is deleted!"}
-
+app.include_router(router)
 
 ### Scalar API Documentation
 @app.get("/scalar", include_in_schema=False)
